04d9f9e3a79ff8ff9919d0ec8b3a01ab14c03b69
John Hill <brashcaran@gmail.com>
2014-06-21 23:00:44 -0700

    have the ITextParser return the correct thing

--- Text Adventure Game/src/jjcard/textGames/game/parser/ITextParser.java
+++ Text Adventure Game/src/jjcard/textGames/game/parser/ITextParser.java
@@ -3,9 +3,9 @@ package jjcard.textGames.game.parser;
 public interface ITextParser<T extends Enum<T> & ITextTokenType> {
 	
 	/**
-	 * 
+	 * Parses the String and returns a TextTokenStream containing the parsed result.
 	 * @param text
 	 */
-	public void parseText(String text);
+	public TextTokenStream<T> parseText(String text);
 
 }

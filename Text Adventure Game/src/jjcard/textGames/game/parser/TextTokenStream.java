5625f0bf61e6aea09d5eb43297aaa6c85871d0cb
John Hill <brashcaran@gmail.com>
2014-06-22 11:30:28 -0700

    added some checking and helper method to TextTokenStream

--- Text Adventure Game/src/jjcard/textGames/game/parser/TextTokenStream.java
+++ Text Adventure Game/src/jjcard/textGames/game/parser/TextTokenStream.java
@@ -1,5 +1,7 @@
 package jjcard.textGames.game.parser;
 
+import java.util.List;
+
 /**
  * 
  * A class to hold to result from the parser in a way easy for the game to use.
@@ -8,34 +10,57 @@ package jjcard.textGames.game.parser;
  */
 public class TextTokenStream<T extends Enum<T> & ITextTokenType> {
 
-	private TextToken<T> object;
+	private List<TextToken<T>> objects;
 	
 	private TextToken<T> verb;
 	
 	private TextToken<T> withObject;
 	
-	public TextTokenStream(TextToken<T> verb, TextToken<T> object){
-		this.object = object;
+	public TextTokenStream(TextToken<T> verb, List<TextToken<T>> objects){
+		if (objects == null){
+			throw new NullPointerException("Objects List must be non null");
+		}
+		this.objects = objects;
 		this.verb = verb;
 	}
-	public TextTokenStream(TextToken<T> verb, TextToken<T> object, TextToken<T> withObject){
-		this.object = object;
+	public TextTokenStream(TextToken<T> verb, List<TextToken<T>> objects, TextToken<T> withObject){
+		if (objects == null){
+			throw new NullPointerException("Objects List must be non null");
+		}
+		this.objects = objects;
 		this.verb = verb;
 		this.withObject = withObject;
 	}
-	
+	/**
+	 * Returns true if the TextTokenStream has a object token in the withObject field.
+	 * @return
+	 */
 	public boolean hasWithObject(){
 		return withObject != null;
 	}
 	
-	public TextToken<T> getObject(){
-		return object;
+	public List<TextToken<T>> getObjects(){
+		return objects;
 	}
 	public TextToken<T> getVerb(){
 		return verb;
 	}
 	public TextToken<T> getWithObject(){
 		return withObject;
+	}
+	/**
+	 * returns true if there is an object token in the objects list.
+	 * @return
+	 */
+	public boolean hasObject(){
+		return objects.size() > 0;
+	}
+	/**
+	 * Returns the first Object token if the list if there is one, null otherwise.
+	 * @return
+	 */
+	public TextToken<T> getFirstObject(){
+		return hasObject()? objects.get(0): null;
 	}
 
 }

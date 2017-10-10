76667d77f1ff02e722f933ba4cf94212c65a8607
jjcard <jjcard@fakeemail.com>
2017-10-09 18:54:34 -0700

    commit to save

---
+++ TextAdventurePackage/src/jjcard/text/game/loot/ILootable.java
@@ -0,0 +1,8 @@
+package jjcard.text.game.loot;
+
+import jjcard.text.game.util.Experimental;
+
+@Experimental
+public interface ILootable {
+
+}
---
+++ TextAdventurePackage/src/jjcard/text/game/loot/IlootItem.java
@@ -0,0 +1,10 @@
+package jjcard.text.game.loot;
+
+import jjcard.text.game.util.Experimental;
+
+@Experimental
+public interface IlootItem {
+	public boolean dropsAlways();
+	public double getProbability();
+//	public boolean
+}
